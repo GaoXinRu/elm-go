86c4e968915ab224b9db1654096b069868f23f25
coco <853100439@qq.com>
2019-05-07 01:20:54 +0800

    添加 acc.vue -- 首页

---
+++ src/components/acc.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <h1>Hello~</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "acc",
+        data() {
+            return {
+                msg: ""
+            }
+        },
+        component: {}
+    }
+</script>
+
+<style scoped>
+
+</style>
